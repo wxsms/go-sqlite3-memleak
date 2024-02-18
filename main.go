63f0dc3cd2ae81bdebbd7c5b349d98761461344c
guokairui <wxsms@foxmail.com>
2024-02-18 10:08:17 +0800

    update

--- main.go
+++ main.go
@@ -7,6 +7,17 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+var insert string
+
+func init() {
+	insert = "INSERT INTO dummy_table (f1,f2,f3,f4,f5,f6,f7,f8,f9,f10) VALUES "
+	for i := 0; i < 9999; i += 1 {
+		insert += fmt.Sprintf(`("%s","%s","%s","%s","%s","%s","%s","%s","%s","%s")`, "1111111111", "1111111111", "1111111111", "1111111111", "1111111111", "1111111111", "1111111111", "1111111111", "1111111111", "1111111111")
+		insert += ","
+	}
+	insert = insert[:len(insert)-1]
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/run", func(c *gin.Context) {
@@ -19,14 +30,6 @@ func main() {
 		if _, err := db.Exec(`CREATE TABLE dummy_table (id INT PRIMARY KEY,f1 VARCHAR(50),f2 VARCHAR(50),f3 VARCHAR(50),f4 VARCHAR(50),f5 VARCHAR(50),f6 VARCHAR(50),f7 VARCHAR(50),f8 VARCHAR(50),f9 VARCHAR(50),f10 VARCHAR(50));`); err != nil {
 			panic(err)
 		}
-
-		insert := "INSERT INTO dummy_table (f1,f2,f3,f4,f5,f6,f7,f8,f9,f10) VALUES "
-		for i := 0; i < 9999; i += 1 {
-			insert += fmt.Sprintf(`("%s","%s","%s","%s","%s","%s","%s","%s","%s","%s")`, "1111111111", "1111111111", "1111111111", "1111111111", "1111111111", "1111111111", "1111111111", "1111111111", "1111111111", "1111111111")
-			insert += ","
-		}
-		insert = insert[:len(insert)-1]
-
 		if _, err := db.Exec(insert); err != nil {
 			panic(err)
 		}
